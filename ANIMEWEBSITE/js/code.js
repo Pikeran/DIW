0e97f462a329b5f720ed0cf0259027379f10fe5c
pikeran <pikcell1994@gmail.com>
2019-01-18 19:06:00 +0100

    SUBIENDO CARPETA WEBSITE ANIME

---
+++ ANIMEWEBSITE/js/code.js
@@ -0,0 +1,25 @@
+function cargar() {
+    console.log("Pagina cargada...");
+    document.getElementById("button-menu").addEventListener("click",menuLateral,false);
+    scrollImagenes();
+}
+
+
+function menuLateral(){
+    var menu = document.getElementById("menu-lateral");
+    
+    if(menu.className == "guardado"){
+        menu.className = "mostrado";
+    }else{
+        menu.className = "guardado";
+    }
+}
+var arrayImagenes = ["./img/noticias_scroll/scroll_01.jpg", "./img/noticias_scroll/scroll_02.jpg", "./img/noticias_scroll/scroll_03.jpg"];
+
+function scrollImagenes(){
+    document.getElementById("scroll-01").style =  "background-image: url("+arrayImagenes[0]+");";
+    document.getElementById("scroll-02").style =  "background-image: url("+arrayImagenes[1]+");";
+    document.getElementById("scroll-03").style =  "background-image: url("+arrayImagenes[2]+");";
+}
+
+window.addEventListener("load", cargar, false);

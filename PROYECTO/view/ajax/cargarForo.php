fc83e5ecba6839c0669683b70c55fc9c7724876d
pikeran <david26_94@hotmail.com>
2019-02-18 10:19:37 +0100

    subiendo consulta imagenex y mensajes foro

--- PROYECTO/view/ajax/cargarForo.php
+++ PROYECTO/view/ajax/cargarForo.php
@@ -6,7 +6,7 @@ $id = $_GET["id"];
 
 require_once("../../config/connect.php");
 
-$cons ="SELECT * FROM `MENSAJES` WHERE id_foro ='$id'";
+$cons ="SELECT mensajes.id_mensaje,mensajes.texto,mensajes.CREATED_AT,mensajes.UPDATED_AT,mensajes.id,mensajes.ID_PADRE,mensajes.id_foro,mensajes.nivel,mensajes.login,usuarios.urlImagen FROM mensajes INNER JOIN usuarios ON mensajes.login = usuarios.login WHERE mensajes.id_foro = '$id'";
 $result = mysqli_query($conn, $cons);
 $data = array();
 
